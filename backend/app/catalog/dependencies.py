0b37ab76ad1338f8dba0869cc2a2a070d0cc751a
Ilya Baburov <tiran555888@gmail.com>
2026-05-25 22:11:48 +0600

    cont stage

--- backend/app/catalog/dependencies.py
+++
@@ -1,6 +0,0 @@
-from backend.app.core.database import SessionDbDep
-from backend.app.catalog.repositories import CategoryRepo, ProductRepo
-from backend.app.catalog.services import CatalogService
-
-def get_catalog_service(session: SessionDbDep) -> CatalogService:
-    return CatalogService(CategoryRepo(session), ProductRepo(session))
